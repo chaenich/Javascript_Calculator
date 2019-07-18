f953fd6f5a6c01cd8cea609bd7bd638c9b1c3a2e
Chae Nicholson <chae.nicholson@gmail.com>
2019-07-18 21:19:07 +0100

    should update the display of the running total when number buttons pressed

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -14,6 +14,32 @@ describe('calculator functionality', function() {
     running_total = element(by.css('#running_total'))
     element(by.css('#number2')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
-  })
+  });
+
+  it('should update the display of the running total', function(){
+    // should update the display of the running total when number buttons pressed
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number5')).click();
+    element(by.css('#number7')).click();
+    element(by.css('#number8')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('578')
+
+  });
+
+  // it('should update the display with the result of the operation after arithmetical operations', function(){
+  //
+  // });
+  // it('should chain multiple operations together', function(){
+  //
+  // });
+  //
+  // it('should provide expected output for a range of numbers', function(){
+  //   // Is the output as expected for a range of numbers (for example, positive, negative, decimals and very large numbers)?
+  //
+  // });
+  //
+  // it('should display Not a number if a numberis divided by zero', function(){
+  //
+  // });
 
 });
