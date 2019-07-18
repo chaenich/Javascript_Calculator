7b138d28a66c94176eadc0b91116f53d0427f4b3
Chae Nicholson <chae.nicholson@gmail.com>
2019-07-18 18:13:36 +0100

    Basic arithmetic function tests implemented

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -10,5 +10,25 @@ describe('calculator', function () {
   it('it has a sample test', function(){
     assert.equal(true, true)
   })
+  it('it should add 1 to 4 and get 5', function(){
+    calculator.previousTotal = 4;
+    calculator.add(1);
+    assert.equal(calculator.runningTotal, "5")
+  });
+  it('it should subtract 4 from 7 and get 3', function(){
+    calculator.previousTotal = 7;
+    calculator.subtract(4);
+    assert.equal(calculator.runningTotal, "3")
+  });
+  it('it should multiply 3 by 5 and get 15', function(){
+    calculator.previousTotal = 3;
+    calculator.multiply(5);
+    assert.equal(calculator.runningTotal, "15")
+  });
+  it('it should divide 21 by 7 and get 3', function(){
+    calculator.previousTotal = 21;
+    calculator.divide(7);
+    assert.equal(calculator.runningTotal, "3")
+  });
 
 });
